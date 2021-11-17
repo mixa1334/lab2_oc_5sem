d82156190957ca38010d057cfee48d00f6233013
mixa1334 <m.kravczov@inbox.ru>
2021-11-17 13:40:35 +0300

    2nd c

--- main.c
+++ main.c
@@ -2,22 +2,38 @@
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 pthread_mutex_t lock;
+sem_t writer;
+sem_t reader;
+
 char symbol;
 
-int main(){
-    pthread_mutex_init(&lock, NULL);
 
-    if(readFile("input.txt") != 0)
-    {
-        printf("cant read from file\n");
-    }
-    printf("\n\n");
-    if(writeFile("output.txt") != 0)
-    {
-        printf("cant write to file\n");
-    }
+int main(){
+
+    pthread_mutex_init(&lock, NULL);
+    sem_init(&writer, 0, 1);
+    sem_init(&reader, 0, 1);
+
+    pthread_t reader_thread, writer_thread;
+
+
+
+    pthread_join(reader_thread, NULL);
+    pthread_join(writer_thread, NULL);
+
+
+    // if(readFile("input.txt") != 0)
+    // {
+    //     printf("cant read from file\n");
+    // }
+    // printf("\n\n");
+    // if(writeFile("output.txt") != 0)
+    // {
+    //     printf("cant write to file\n");
+    // }
 
     return 0;
 }
@@ -27,18 +43,19 @@ int readFile(char *fileName)
     printf("file -> %s\n", fileName);
     FILE *file;
     file = fopen(fileName, "r");
-    char c;
 
     if (file == NULL)
     {
         return -1;
     }
 
-
-    while ((c = fgetc(file)) != EOF)
+    while (1)
     {
-        printf("%c", c);
-        symbol = c;
+        symbol = fgetc(file);
+        if(symbol == EOF){
+            break;
+        }
+        printf("%c", symbol);
     }
 
     fclose(file);
@@ -57,9 +74,12 @@ int writeFile(char *fileName)
         return -1;
     }
 
-    for (size_t i = 0; i < 5; i++)
+    while (1)
     {
         printf("%c", symbol);
+        if(symbol == EOF){
+            break;
+        }
         fputc(symbol, file);
     }
     
